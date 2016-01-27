4d979c8ddd14ac7ab5ef2a232e7e2cf04ec948e7
Ryan <ryanhuebert@gmail.com>
2016-01-27 13:28:19 -0800

    Initial Commit

---
+++ pokedex-by-ryanhuebert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-ryanhuebert
+//
+//  Created by Ryan Huebert on 1/27/16.
+//  Copyright © 2016 Ryan Huebert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
